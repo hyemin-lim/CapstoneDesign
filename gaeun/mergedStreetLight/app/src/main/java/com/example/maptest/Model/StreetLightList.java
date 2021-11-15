702235f93707b9b4e65c0586931f9303c466e12c
KimGaEun <imgaeun@gmail.com>
2021-11-15 13:46:58 +0900

    "어두울 때" 패턴에 맞춰 고침. 재업로드.

---
+++ gaeun/mergedStreetLight/app/src/main/java/com/example/maptest/Model/StreetLightList.java
@@ -0,0 +1,8 @@
+package com.example.maptest.Model;
+
+
+import java.util.ArrayList;
+
+public class StreetLightList {
+    public ArrayList<StreetLight> street_light = new ArrayList<StreetLight>();
+}
